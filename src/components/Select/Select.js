dbc5d3a93ad62cb0da5c688f6713e4c8e93bba26
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2022-01-18 22:31:11 -0800

    Add Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,56 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  opacity: 0;
+  height: 100%;
+  width: 100%;
+`;
+
+const PresentationalContainer = styled.div`
+  background: ${COLORS.transparentGray15};
+  padding: 16px 52px 16px 12px;
+  border-radius: 8px;
+  color: inherit;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 12px;
+  bottom: 0;
+  margin: auto;
+  height: min-content;
+  color: inherit;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalContainer>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" size={16} strokeWidth={2}></Icon>
+        </IconWrapper>
+      </PresentationalContainer>
+    </Wrapper>
   );
 };
 
